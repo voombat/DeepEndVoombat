6f85da2c3f3a5fa5de89a3e45dda5aa04764ae3b
ERCfalse <parshin_ss@mail.ru>
2014-05-10 12:15:52 +0600

    3

--- src/App/DeepEndVoombat.java
+++ src/App/DeepEndVoombat.java
@@ -8,7 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-
+//We in Git Baby
 public class DeepEndVoombat {
 
    
